645f8bb7717ae4fe2bf60500097b58fec7dbc802
QADmitriy <ddigeolog@tut.by>
2021-12-20 16:48:07 +0300

    addition three

--- ch1/tasks/test_three.py
+++ ch1/tasks/test_three.py
@@ -11,7 +11,6 @@ def test_defaults():
     t1 = Task()
     t2 = Task(None, None, False, None)
     assert t1 == t2
-    assert t1 == t2
 
 
 def test_member_access():
@@ -19,5 +18,4 @@ def test_member_access():
     t = Task('buy milk', 'brian')
     assert t.summary == 'buy milk'
     assert t.owner == 'brian'
-    assert (t.done, t.id) == (False, None)
     assert (t.done, t.id) == (False, None)
